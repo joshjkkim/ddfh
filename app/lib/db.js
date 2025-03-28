4fed01984080d91774064ac808447d4e97f711ed
Joshua Kim <jjkk@umich.edu>
2025-03-28 00:09:26 -0400

    ssl enforcement

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: true },
   });
   
 export default pool;
